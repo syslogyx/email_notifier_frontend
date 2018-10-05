b156d2eaf58fc76d164014c3c97f2f38ad28bd07
sonal <sonal.kesare@syslogic.com>
2018-10-05 14:40:12 +0530

    added controller funcitonality

--- controller/createDeviceCtrl.js
+++ controller/createDeviceCtrl.js
@@ -1,36 +1,38 @@
 app.controller('createDeviceCtrl', function ($scope,menuService,services,$cookieStore,$location) {
 
 	var dmcc = this;
-	dmcc.title = "Add New Device";
-	dmcc.id = null;
+	dmcc.userId = null;
+    dmcc.title = "Add New Device";
 	console.log(dmcc.title);
-	// dmcc.userId = $location.search()['id'];
 
-	
-	var loggedInUser = JSON.parse(services.getIdentity());
-	
-	dmcc.init = function(){
-		// if(dmcc.userId){
-		// 	dmcc.title = "Update Device";
-		// }
-		// console.log(dmcc.title);
+    
+    var loggedInUser = JSON.parse(services.getIdentity());
+    
+	dmcc.userId = $location.search()['id'];
+
+    dmcc.init = function(){
+		if(dmcc.userId > 0){
+            var promise = services.getDeviceById(id);
+            promise.success(function (result) {
+                Utility.stopAnimation();
+                if(result.status_code == 200){
+                    dmcc.id = result.data.id;
+                    dmcc.deviceName = result.data.name;
+                    dmcc.title = "Update Device";
+                    // $("#addDeviceModal").modal("toggle");
+                }else{
+                    toastr.error(result.message, 'Sorry!');
+                }
+            });
+			
+		}
+		console.log(dmcc.title);
 		dmcc.getReasonList();
 	}
 
-	dmcc.getDeviceData = function (id) {
-        var promise = services.getDeviceById(id);
-        promise.success(function (result) {
-            Utility.stopAnimation();
-            if(result.status_code == 200){
-                dmcc.id = result.data.id;
-                dmcc.deviceName = result.data.name;
-                dmcc.title = "Update Device";
-                $("#addDeviceModal").modal("toggle");
-            }else{
-                toastr.error(result.message, 'Sorry!');
-            }
-        });
-    }
+	// dmcc.getDeviceData = function (id) {
+        
+    //    }
 
 	dmcc.saveDevice = function () {
         if ($("#addDeviceForm").valid()) {
@@ -74,7 +76,7 @@ app.controller('createDeviceCtrl', function ($scope,menuService,services,$cookie
             if(result.status_code == 200){
                 Utility.stopAnimation();
 				dmcc.reasonList = result.data;
-				// console.log(macc.reasonList);
+				console.log(dmcc.reasonList);
             }else{
 				Utility.stopAnimation();
 				dmcc.reasonList = [];
@@ -89,9 +91,34 @@ app.controller('createDeviceCtrl', function ($scope,menuService,services,$cookie
             $(this).removeClass('has-error');
             $('span.help-block-error').remove();
         });
-        dmc.id = null;
-        dmc.deviceName = '';
+        dmcc.id = null;
+        dmcc.deviceName = '';
     };
+
+    dmcc.addCustomReason = function(){
+        console.log("test");
+        // var req = {
+        //         "status":statusType,
+        //         "reason": $scope.new_on_reason
+        //     }
+        //     var promise = services.saveReason(req);
+        //     promise.then(function mySuccess(result) {
+        //         Utility.stopAnimation();
+        //         // console.log(result);
+        //         if(result.data.status_code == 200){                    
+        //             dmc.getReasonList();
+        //             // $("#addCustomReason_on").modal('hide');
+        //             toastr.success(result.data.message);
+        //         }else{
+        //             toastr.error(result.data.errors.email[0], 'Sorry!');
+        //         }
+
+        //     }, function myError(r) {
+        //         toastr.error(r.data.errors.email[0], 'Sorry!');
+        //         Utility.stopAnimation();
+        //         // $("#addCustomReason_on").modal('show');
+        //     });
+    }
 
     dmcc.init();
 	   
